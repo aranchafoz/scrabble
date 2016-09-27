bd36bd40bc512c957fd86430145d7d77c4a8e5b9
enriquemc96 <enriquemc96@gmail.com>
2016-09-27 13:23:38 +0800

    Added OccupiedCellException and WrongWordException classes

---
+++ src/scrabble/exceptions/WrongWordException.java
@@ -0,0 +1,17 @@
+package scrabble.exceptions;
+
+/**
+ * Created by enrique on 27/09/16.
+ */
+public class WrongWordException extends Exception {
+    private String word;
+
+    public WrongWordException(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error, the word " + word + " does not exist";
+    }
+}
