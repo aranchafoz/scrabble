3a44f8c16d6409529cc85af52875b2f0947a49e1
ivorra <divorrap@gmail.com>
2016-09-30 14:56:58 +0800

    atributes of cell created

--- src/main/scrabble/model/Cell.java
+++ src/main/scrabble/model/Cell.java
@@ -4,4 +4,11 @@ package test.scrabble.model.scrabble.model;
  * Created by enrique on 27/09/16.
  */
 public class Cell {
+    private Piece piece;
+    private int x;
+    private int y;
+    private CellType type;
+
+
+
 }
