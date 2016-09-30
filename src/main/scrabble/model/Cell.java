ca8e5e482aefe231268d537212820bea8eaf6c47
ivorra <divorrap@gmail.com>
2016-09-30 16:45:42 +0800

    Added some Cell methods and exception

--- src/main/scrabble/model/Cell.java
+++ src/main/scrabble/model/Cell.java
@@ -4,11 +4,37 @@ package test.scrabble.model.scrabble.model;
  * Created by enrique on 27/09/16.
  */
 public class Cell {
+
     private Piece piece;
     private int x;
     private int y;
     private CellType type;
 
+  public Cell() {
+      piece = new Piece();
+      x = 0;
+      y = 0;
+      type = CellType.plain
+  }
 
+ public Cell(Piece piece, int x, int y, CellType type) throws WrongCoordinateException {
+     if(checkCoordinates(x,y)) {
+         this.piece = piece;
+         this.x = x;
+         this.y = y;
+         this.type = type;
+     } else {
+         throws new WrongCoordinateException(x,y);
+     }
+ }
 
+ private boolean checkCoordinates(int xCoord, int yCoord) {
+     boolean correct = true;
+     if(xCoord < 0 || yCoord < 0){
+         if(xCoord > 14) || yCoord > 14) {
+             correct = false;
+         }
+     }
+     return coorrect;
+ }
 }
